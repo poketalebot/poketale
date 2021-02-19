3a5c10ef52964169d2c99e029e6aafa214781a0e
Ash <65588168+ashpotter@users.noreply.github.com>
2021-02-19 19:58:47 +0300

    Update botum.js

--- botum.js
+++ botum.js
@@ -9,12 +9,18 @@ botum.on('ready', async () => {
   console.log("Sa yardım my botum aktif oldu")
 });
 
-// Annen
 botum.on("message", async (message,params,args) => {
   	let command = msg.content.split(' ')[0].slice(prefix.length);
     if(command === "yardım") {
     msg.channel.send("sa yardım my botum")
     }
+   if(command === "") {
+    msg.channel.send("KoMUt bUluNaMaDI")
+    }
+  if(command === "token"){
+   msg.channel.send("EğLeNCe KOmuTlarInı KuLlAnmak İçİN boTa Oy VeRMeLİSiN!!!!111!)
+  }
+ 
 })
 
 botum.login("tokenim")
